b393d2716e8a74d14709d12b9b149f6b324e21c7
Deni Setiadi <halloobeer@gmail.com>
2026-08-29 00:53:51 +0700

    Load admin auth before application scripts

--- api/page.js
+++ api/page.js
@@ -5,7 +5,9 @@ module.exports = async function handler(req, res) {
   try {
     const file = path.join(process.cwd(), 'index.html');
     let html = fs.readFileSync(file, 'utf8');
-    html = html.replace('<script src="./app.js"></script>', '<script src="./app.js"></script>\n  <script src="./db-sync.js"></script>');
+    if (!html.includes('./auth-ui.js')) {
+      html = html.replace('<script src="./app.js"></script>', '<script src="./auth-ui.js"></script>\n  <script src="./app.js"></script>');
+    }
     res.setHeader('Content-Type', 'text/html; charset=utf-8');
     res.setHeader('Cache-Control', 'no-store');
     return res.status(200).send(html);
