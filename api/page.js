2be6fd2decf1b2a8ef34f51e7cf2be3d2010ec84
Deni Setiadi <halloobeer@gmail.com>
2026-08-28 23:43:56 +0700

    Inject database client into app page

---
+++ api/page.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = async function handler(req, res) {
+  try {
+    const file = path.join(process.cwd(), 'index.html');
+    let html = fs.readFileSync(file, 'utf8');
+    html = html.replace('<script src="./app.js"></script>', '<script src="./app.js"></script>\n  <script src="./db-sync.js"></script>');
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    res.setHeader('Cache-Control', 'no-store');
+    return res.status(200).send(html);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send('Gagal memuat aplikasi');
+  }
+};
